138e4b249b64928de0cb8ee720cb3ea4268d8381
Tahsin Ayman <mail4tahsin@gmail.com>
2021-07-23 22:25:15 +0600

    Diamond

--- App.c
+++ App.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    printf ("Hello world...");
+    int a[5][5] = {
+                    {32,32,42,32,32},
+                    {32,42,42,42,32},
+                    {42,42,42,42,42},
+                    {32,42,42,42,32},
+                    {32,32,42,32,32}
+                                        };
+    for (int row =  0; row < 5; row++){
+        for (int col = 0; col < 5; col++){
+            printf ("%c" , a[row][col]);
+        }
+        printf ("\n");
+    }
     return 0;   
 }
