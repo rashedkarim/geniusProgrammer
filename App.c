bc6d521de283ae86856af5051437070c0948fc9c
Tahsin Ayman <mail4tahsin@gmail.com>
2021-07-23 23:07:40 +0600

    DiamondFinalize

--- App.c
+++ App.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
+#include "Diamond.c"
 
 int main(){
-    int matrix;
-    printf ("Enter the size of square: ");
-    scanf ("%d" , &matrix);
-
-    for(int row = 1; row < matrix; row++){
-        for(int col = 1; col < matrix; col++){
-            printf (" * ");
-        }
-        printf ("\n");
-    }
+    diamond();
     return 0;
 }
