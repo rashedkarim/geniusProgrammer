3b3a095c68e45c8aa96f24edffbb919dddd4f062
Tahsin Ayman <mail4tahsin@gmail.com>
2021-07-24 20:59:08 +0600

    A new header file

--- App.c
+++ App.c
@@ -1,7 +1,12 @@
 #include <stdio.h> 
 #include "Practice.c"
+#include "loop.c"
+//#include "pivot.c"
 
 int main(){
-    practice();
+    int a;
+    printf ("Enter the size to for loop: ");
+    scanf ("%d" , &a);
+    loop(a);
     return 0;
 }
---
+++ loop.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int loop(int loop){
+    for (int l = 1; l <= loop; l++){
+        printf ("%4d" , l);
+    }
+    return 0;
+}
