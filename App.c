fdb8ee03762d038c7253dec6cfea9cdd80322b15
Rashed Karim <rashedmkarim@gmail.com>
2021-07-24 13:14:52 +0800

    set matrix = 5

--- App.c
+++ App.c
@@ -5,14 +5,11 @@
 #include "lib/myHeader.h"
 
 int main(){
-    int matrix;
+    int matrix = 5;
     intInput ("Enter the size of square: ", "%d", &matrix);
-    scanf ("%d" , &matrix);
 
     box(matrix);
-    int c = add(5,6);
-    printf("\n%d", c);
-
+    
     diamond();
     
     printf ("\nPress any key to exit");
--- Diamond.c
+++ Diamond.c
@@ -7,11 +7,12 @@ void diamond(){
                     {42,42,42,42,42},
                     {32,42,42,42,32},
                     {32,32,42,32,32}
-                                        };
+    };
     for (int row = 0; row < 5; row++){
+        printf ("\n");
         for (int col = 0; col < 5; col++){
             printf ("%c" , a[row][col]);
         }
-        printf ("\n");
+        
     }
 }
