6497a816657f65fe5c7237cd23aa5d58cd9d8c73
Tahsin Ayman <mail4tahsin@gmail.com>
2021-07-29 18:29:54 +0600

    noting

--- Practice.c
+++ Practice.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int practice(){
+int practice(){                                                                                                                                                                                                                         system("color 0a");
     printf ("Hello world...");
     return 0;
 }
