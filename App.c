d625ac6f6665a394a76c2fb1301bce7dbe396520
TahsinAyman <mail4tahsin@gmail.com>
2021-08-08 14:35:54 +0600

    nothing

--- App.c
+++ App.c
@@ -2,28 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <conio.h>
-<<<<<<< HEAD
 #include "color.h"
-=======
 #include "delay.h"
->>>>>>> c1e93fbde7dd6dab40192ea891d420ecfc258d32
 #include "practice.c"
 #include "BinarySearch.c"
 #include "LinearSearch.c"
 #include "lib/myHeader.h"
 
 int main(){
-<<<<<<< HEAD
-    color();
-    practice ();
-=======
-    string str;
-
-    str = "Tahsin Ayman";
-    
-    printf("%s ", str );
-
-    printf("End of the code");
->>>>>>> c1e93fbde7dd6dab40192ea891d420ecfc258d32
+	printf ("Hello world...");
     return 0;
 }
